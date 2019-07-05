00a93a1b1e5d4c7ed343cd4f8ccbb5a4530024c3
Dragos <dragosct10@gmail.com>
2019-07-05 14:09:18 +0300

    copyright on html, js, scss

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+// =========================================================
+// * Vue Material Kit - v1.1.0
+// =========================================================
+//
+// * Product Page: https://www.creative-tim.com/product/vue-material-kit
+// * Copyright 2019 Creative Tim (https://www.creative-tim.com)
+// * Licensed under MIT (https://github.com/creativetimofficial/vue-material-kit/blob/master/LICENSE.md)
+//
+// * Coded by Creative Tim
+//
+// =========================================================
+//
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
