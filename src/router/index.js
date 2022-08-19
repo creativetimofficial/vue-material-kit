dc48cc9694325cfd01486c1691e6ba1dd10a3d1c
sajadevo <nawabisajadahmad@gmail.com>
2022-08-19 17:40:32 +0430

    ready for deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import PresentationView from "../views/Presentation/PresentationView.vue";
 import AboutView from "../views/LandingPages/AboutUs/AboutView.vue";
 import ContactView from "../views/LandingPages/ContactUs/ContactView.vue";
@@ -24,129 +24,129 @@ import ElProgressBars from "../layouts/sections/elements/progress-bars/ProgressB
 import ElToggles from "../layouts/sections/elements/toggles/TogglesView.vue";
 import ElTypography from "../layouts/sections/elements/typography/TypographyView.vue";
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
       name: "presentation",
-      component: PresentationView,
+      component: PresentationView
     },
     {
       path: "/pages/landing-pages/about-us",
       name: "about",
-      component: AboutView,
+      component: AboutView
     },
     {
       path: "/pages/landing-pages/contact-us",
       name: "contactus",
-      component: ContactView,
+      component: ContactView
     },
     {
       path: "/pages/landing-pages/author",
       name: "author",
-      component: AuthorView,
+      component: AuthorView
     },
     {
       path: "/pages/landing-pages/basic",
       name: "signin-basic",
-      component: SignInBasicView,
+      component: SignInBasicView
     },
     {
       path: "/sections/page-sections/page-headers",
       name: "page-headers",
-      component: PageHeaders,
+      component: PageHeaders
     },
     {
       path: "/sections/page-sections/features",
       name: "page-features",
-      component: PageFeatures,
+      component: PageFeatures
     },
     {
       path: "/sections/navigation/navbars",
       name: "navigation-navbars",
-      component: NavigationNavbars,
+      component: NavigationNavbars
     },
     {
       path: "/sections/navigation/nav-tabs",
       name: "navigation-navtabs",
-      component: NavigationNavTabs,
+      component: NavigationNavTabs
     },
     {
       path: "/sections/navigation/pagination",
       name: "navigation-pagination",
-      component: NavigationPagination,
+      component: NavigationPagination
     },
     {
       path: "/sections/input-areas/inputs",
       name: "inputareas-inputs",
-      component: InputAreasInputs,
+      component: InputAreasInputs
     },
     {
       path: "/sections/input-areas/forms",
       name: "inputareas-forms",
-      component: InputAreasForms,
+      component: InputAreasForms
     },
     {
       path: "/sections/attention-catchers/alerts",
       name: "ac-alerts",
-      component: ACAlerts,
+      component: ACAlerts
     },
     {
       path: "/sections/attention-catchers/modals",
       name: "ac-modals",
-      component: ACModals,
+      component: ACModals
     },
     {
       path: "/sections/attention-catchers/tooltips-popovers",
       name: "ac-tooltips-popovers",
-      component: ACTooltipsPopovers,
+      component: ACTooltipsPopovers
     },
     {
       path: "/sections/elements/avatars",
       name: "el-avatars",
-      component: ElAvatars,
+      component: ElAvatars
     },
     {
       path: "/sections/elements/badges",
       name: "el-badges",
-      component: ElBadges,
+      component: ElBadges
     },
     {
       path: "/sections/elements/breadcrumbs",
       name: "el-breadcrumbs",
-      component: ElBreadcrumbs,
+      component: ElBreadcrumbs
     },
     {
       path: "/sections/elements/buttons",
       name: "el-buttons",
-      component: ElButtons,
+      component: ElButtons
     },
     {
       path: "/sections/elements/button-groups",
       name: "el-button-groups",
-      component: ElButtonGroups,
+      component: ElButtonGroups
     },
     {
       path: "/sections/elements/dropdowns",
       name: "el-dropdowns",
-      component: ElDropdowns,
+      component: ElDropdowns
     },
     {
       path: "/sections/elements/progress-bars",
       name: "el-progress-bars",
-      component: ElProgressBars,
+      component: ElProgressBars
     },
     {
       path: "/sections/elements/toggles",
       name: "el-toggles",
-      component: ElToggles,
+      component: ElToggles
     },
     {
       path: "/sections/elements/typography",
       name: "el-typography",
-      component: ElTypography,
-    },
-  ],
+      component: ElTypography
+    }
+  ]
 });
 
 export default router;
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/vue-material-kit-pro/",
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  }
 });
