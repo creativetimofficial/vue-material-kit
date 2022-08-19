5fef4d22f64739fb1fa7e703d1dfe081e6449628
sajadevo <nawabisajadahmad@gmail.com>
2022-08-19 17:51:16 +0430

    ready for deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/vue-material-kit-pro/",
+  base: "/vue-material-kit/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
