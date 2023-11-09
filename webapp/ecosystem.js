3d35dbd8a5b8969080f25ac60accb979473359e7
kamoa457 <kamoa457@cubes.postbox.in.th>
2023-11-09 15:20:03 +0700

    add syster

---
+++ webapp/ecosystem.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+      {
+        name: "web",
+        script: "./node_modules/@vue/cli-service/bin/vue-cli-service.js",
+        args: "serve"
+      }
+    ]
+  };
